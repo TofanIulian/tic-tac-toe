251da5e7dfb64c3c45cd8f15aed0403fe2c1ae2b
Iulian Tofan <itn@tanacorp.com>
2022-02-16 00:27:46 +0200

    Added Nebular theming to the app.

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,11 +2,20 @@ import { Component, Input } from '@angular/core';
 import { Player } from '../player';
 
 /**
- * A dumb component with the sole purpose of displaying a button in a square-like shape.
+ * A dumb component with the sole purpose of displaying a button in a square-like shape
+ * and, in case that is it marked, the player that marked it.
  */
 @Component({
   selector: 'app-square',
-  template: ` <button>{{ value }}</button> `,
+  template: `
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton *ngIf="value === playerX" hero status="success">
+      {{ value }}
+    </button>
+    <button nbButton *ngIf="value === playerO" hero status="info">
+      {{ value }}
+    </button>
+  `,
   styles: [
     `
       button {
@@ -19,4 +28,12 @@ import { Player } from '../player';
 })
 export class SquareComponent {
   @Input() value: Player;
+
+  public get playerX() {
+    return Player.X;
+  }
+
+  public get playerO() {
+    return Player.O;
+  }
 }
